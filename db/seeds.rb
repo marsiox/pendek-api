962c7fd1952e2a266721d495ae9bf4d325c36d3a
Krzyś Marszałek <krzys.marszalek@gmail.com>
2017-08-19 20:11:19 +0700

    DB seeds - sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+sessions = []
+20.times do
+  sessions << Session.create({
+    user_session_id: SecureRandom.hex,
+    ip_address: Faker::Internet.ip_v4_address,
+    http_referer: Faker::Internet.url('testreferer.com'),
+    user_agent: Faker::Internet.user_agent
+  })
+end
+
+100.times do
+  Url.create(
+    session_id: sessions[rand(sessions.count)].id,
+    full: Faker::Internet.url,
+    short: Pendek::ShortURL.print
+  )
+end
