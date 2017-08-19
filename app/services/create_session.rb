800e78435e319ad75adf7d468d4fdb994d0415a7
Krzyś Marszałek <krzys.marszalek@gmail.com>
2017-08-20 00:33:14 +0700

    Created SaveSession service

---
+++ app/services/create_session.rb
@@ -0,0 +1,21 @@
+class CreateSession
+  include Service
+  include ActiveModel::Validations
+
+  def initialize(request_headers, url)
+    @request_headers = request_headers
+    @url = url
+  end
+
+  def run
+    session = Session.new
+    session.user_session_id = @request_headers["User-Session-Id"]
+    session.ip_address = @request_headers["User-Ip-Addr"]
+    session.http_referer = @request_headers["User-Referer"]
+    session.user_agent = @request_headers["User-Agent"]
+    session.url = @url
+    session.save
+    session if session.persisted?
+  end
+
+end
