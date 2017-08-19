66bc883b3b0610e15c4dcd271b39ed2cdade7cb2
Krzyś Marszałek <krzys.marszalek@gmail.com>
2017-08-19 17:57:52 +0700

    Service object for url create

---
+++ app/services/create_short_url.rb
@@ -0,0 +1,17 @@
+class CreateShortUrl
+  include Service
+  include ActiveModel::Validations
+
+  def initialize(params)
+    @params = params
+  end
+
+  def run
+    url = Url.new
+    url.full = @params["full"]
+    url.short = Pendek::ShortURL.print
+    url.save
+    url if url.persisted?
+  end
+
+end
